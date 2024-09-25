647c726557bcba50e422c6facc56537e2035b5f5
Ayal_Z <ayal.zingher@perkinelmer.com>
2024-09-25 17:04:28 -0400

    Slicer files added

---
+++ AZImager/Applications/AZImagerApp/slicer-application-properties.cmake
@@ -0,0 +1,35 @@
+
+set(APPLICATION_NAME
+  AZImager
+  )
+
+set(VERSION_MAJOR
+  0
+  )
+set(VERSION_MINOR
+  1
+  )
+set(VERSION_PATCH
+  0
+  )
+
+set(DESCRIPTION_SUMMARY
+  "Practice app usinmg 3DSlicer"
+  )
+set(DESCRIPTION_FILE
+  ${Slicer_SOURCE_DIR}/README.md
+  )
+
+set(LAUNCHER_SPLASHSCREEN_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Images/SplashScreen.png"
+  )
+set(APPLE_ICON_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.icns"
+  )
+set(WIN_ICON_FILE
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.ico"
+  )
+
+set(LICENSE_FILE
+  "${AZImager_SOURCE_DIR}/LICENSE"
+  )
